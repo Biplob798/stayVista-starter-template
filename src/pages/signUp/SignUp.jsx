b34f9889aaa33e9019697133b565925e5f452c74
Biplob798 <mdbiplop798@gmail.com>
2023-11-16 13:55:50 +0600

    signUp user with google and save jwt token

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,8 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
-import axios from "axios";
+import { imageUpload } from "../../api/utils";
+
+import useAuth from "../../hooks/useAuth";
+import { getToken, saveUser } from "../../api/auth";
+import toast from "react-hot-toast";
+import { TbFidgetSpinner } from "react-icons/tb";
 
 const SignUp = () => {
+  const { user, loading, createUser, updateUserProfile, signInWithGoogle } =
+    useAuth();
+  const navigate = useNavigate();
   // form submit handle
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -11,23 +19,50 @@ const SignUp = () => {
     const email = form.email.value;
     const password = form.password.value;
     const image = form.image.files[0];
-    const formData = new FormData();
-    formData.append("image", image);
+    // const imageData = await imageUpload(image);
+    // console.log(imageData);
+    console.log(name, email, password);
+
     try {
-      const { data } = await axios.post(
-        `https://api.imgbb.com/1/upload?key=${
-          import.meta.env.VITE_IMAGE_API_KEY
-        }`,
-        formData
-      );
-      console.log(data);
+      // 1.upload image
+      const imageData = await imageUpload(image);
+      //2. user Register
+      const result = await createUser(email, password);
+      //3. save userName & profile photo
+      await updateUserProfile(name, imageData?.data?.display_url);
+      // 4.save user data in data base
+      const dbResponse = await saveUser(result?.user);
+      console.log(dbResponse);
+      // result.user.email
+      console.log(result);
+      //5. get token
+      await getToken(result?.user?.email);
+      navigate("/");
+      toast.success("signUp successfully");
     } catch (error) {
       console.log(error);
+      toast.error(error?.message);
     }
+  };
 
-    console.log(formData);
-    console.log({ name, email, password });
-    console.log(image);
+  // Handle google sign in
+  const handleGoogleSignIn = async () => {
+    try {
+      //1. user Register using google
+      const result = await signInWithGoogle();
+      // 2.save user data in data base
+      const dbResponse = await saveUser(result?.user);
+      console.log(dbResponse);
+      // result.user.email
+      console.log(result);
+      //3. get token
+      await getToken(result?.user?.email);
+      navigate("/");
+      toast.success("signUp successfully");
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.message);
+    }
   };
 
   return (
@@ -106,7 +141,11 @@ const SignUp = () => {
               type="submit"
               className="bg-rose-500 w-full rounded-md py-3 text-white"
             >
-              Continue
+              {loading ? (
+                <TbFidgetSpinner className="animate-spin m-auto"></TbFidgetSpinner>
+              ) : (
+                "Continue"
+              )}
             </button>
           </div>
         </form>
@@ -117,7 +156,10 @@ const SignUp = () => {
           </p>
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
         </div>
-        <div className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer">
+        <div
+          onClick={handleGoogleSignIn}
+          className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer"
+        >
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
